1dc369b5f75ee45fbac5c5504274e6c909a6882b
André Josefsson <andre.v.json@gmail.com>
2019-07-02 20:29:50 +0200

    Add project files.

---
+++ JosPot/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JosPot
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            button.Clicked += Button_Clicked;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string something = entry.Text;
+            if(string.IsNullOrEmpty(something))
+                return;
+            await DisplayAlert("WAWAWAWA", $"Is '{something}' all you have to say for yourself!?", "Ya");
+            label.Text = $"Say something other than '{something}'!";
+            entry.Text = string.Empty;
+        }
+    }
+}
