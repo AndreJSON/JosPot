e13b218e37c8f282c8141506ef3f52458f75c8c9
Johan Potrus <johan.potrus@live.se>
2019-07-03 19:42:07 +0200

    Changed to canvas stuff

--- JosPot/MainPage.xaml.cs
+++ JosPot/MainPage.xaml.cs
@@ -5,26 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 
 namespace JosPot
 {
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        SKPaint blackFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Black
+        };
         public MainPage()
         {
             InitializeComponent();
-            button.Clicked += Button_Clicked;
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private void CanvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
-            string something = entry.Text;
-            if(string.IsNullOrEmpty(something))
-                return;
-            await DisplayAlert("WAWAWAWA", $"Is '{something}' all you have to say for yourself!?", "Ya");
-            label.Text = $"Say something other than '{something}'!";
-            entry.Text = string.Empty;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.CornflowerBlue);
+
+            canvas.DrawCircle(0, 0, 100, blackFillPaint);
+
         }
     }
 }
