da987748016990b53f5da6373eea23dd51c96811
Johan Potrus <johan.potrus@live.se>
2019-07-03 21:52:06 +0200

    made a cat watch without animation

--- JosPot/MainPage.xaml.cs
+++ JosPot/MainPage.xaml.cs
@@ -18,9 +18,74 @@ namespace JosPot
             Style = SKPaintStyle.Fill,
             Color = SKColors.Black
         };
+
+        SKPaint whiteStrokePaint = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.White,
+            StrokeWidth = 2,
+            StrokeCap = SKStrokeCap.Round,
+            IsAntialias = true
+        };
+
+        SKPaint whiteFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.White
+        };
+
+        SKPaint greenFillPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.PaleGreen
+        };
+
+        SKPaint blackStrokePaint = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.Black,
+            StrokeWidth = 20,
+            StrokeCap=SKStrokeCap.Round
+
+        };
+
+
+        SKPath catEarPath = new SKPath();
+        SKPath catEyePath = new SKPath();
+        SKPath catPupilPath = new SKPath();
+        SKPath catTailPath = new SKPath();
+
         public MainPage()
         {
             InitializeComponent();
+
+            catEarPath.MoveTo(0, 0);
+            catEarPath.LineTo(0, 75);
+            catEarPath.LineTo(100, 75);
+            catEarPath.Close();
+
+            catEyePath.MoveTo(0, 0);
+            catEyePath.ArcTo(50, 50, 0, SKPathArcSize.Small, SKPathDirection.Clockwise, 50, 0);
+            catEyePath.ArcTo(50, 50, 0, SKPathArcSize.Small, SKPathDirection.Clockwise, 0, 0);
+            catEyePath.Close();
+
+            catPupilPath.MoveTo(25, -5);
+            catPupilPath.ArcTo(6, 6, 0, SKPathArcSize.Small, SKPathDirection.Clockwise, 25, 5);
+            catPupilPath.ArcTo(6, 6, 0, SKPathArcSize.Small, SKPathDirection.Clockwise, 25, -5);
+            catPupilPath.Close();
+
+            catTailPath.MoveTo(0, 100);
+            catTailPath.CubicTo(50, 200, 0, 250, -50, 200);
+
+
+
+
+
+            Device.StartTimer(TimeSpan.FromSeconds(1f / 60), () =>
+               {
+                   CanvasView.InvalidateSurface();
+                   return true;
+               });
         }
 
         private void CanvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
@@ -30,8 +95,76 @@ namespace JosPot
 
             canvas.Clear(SKColors.CornflowerBlue);
 
-            canvas.DrawCircle(0, 0, 100, blackFillPaint);
+            int width = e.Info.Width;
+            int height = e.Info.Height;
 
+            // Set transforms
+            canvas.Translate(width / 2, height / 2);
+            canvas.Scale(Math.Min(width/210f,height/520f));
+
+            //Get DateTime
+            DateTime dateTime = DateTime.Now;
+
+            //Head
+            canvas.DrawCircle(0, -160, 75, blackFillPaint);
+
+            for (int i = 0; i < 2; i++)
+            {
+                canvas.Save();
+                canvas.Scale(2 * i - 1, 1);
+
+                canvas.Save();
+                canvas.Translate(-65, -255);
+                canvas.DrawPath(catEarPath, blackFillPaint);
+                canvas.Restore();
+
+                canvas.Save();
+                canvas.Translate(10, -170);
+                canvas.DrawPath(catEyePath, greenFillPaint);
+                canvas.DrawPath(catPupilPath, blackFillPaint);
+                canvas.Restore();
+
+                canvas.DrawLine(10, -120, 100, -100, whiteStrokePaint);
+                canvas.DrawLine(10, -125, 100, -120, whiteStrokePaint);
+                canvas.DrawLine(10, -130, 100, -140, whiteStrokePaint);
+                canvas.DrawLine(10, -135, 100, -160, whiteStrokePaint);
+
+                canvas.Restore();
+
+            }
+
+
+            canvas.DrawPath(catTailPath, blackStrokePaint);
+                //Clock background
+                canvas.DrawCircle(0, 0, 100, blackFillPaint);
+
+            //Hour and minute marks
+            for (int angle = 0; angle < 360; angle += 6)
+            {
+                canvas.DrawCircle(0, -90, angle % 30 == 0 ? 4 : 2, whiteFillPaint);
+                canvas.RotateDegrees(6);
+            }
+
+            //Hour hand
+            canvas.Save();
+            canvas.RotateDegrees(30 * dateTime.Hour + dateTime.Minute / 2f);
+            whiteStrokePaint.StrokeWidth = 15;
+            canvas.DrawLine(0, 0, 0, -50, whiteStrokePaint);
+            canvas.Restore();
+
+            canvas.Save();
+            canvas.RotateDegrees(6 * dateTime.Minute + dateTime.Second / 10f);
+            whiteStrokePaint.StrokeWidth = 10;
+            canvas.DrawLine(0, 0, 0, -70, whiteStrokePaint);
+            canvas.Restore();
+
+            //Second hand
+            canvas.Save();
+            float seconds = dateTime.Second + dateTime.Millisecond / 1000f;
+            canvas.RotateDegrees(6 * seconds);
+            whiteStrokePaint.StrokeWidth = 2;
+            canvas.DrawLine(0, 10, 0, -80, whiteStrokePaint);
+            canvas.Restore();
         }
     }
 }
